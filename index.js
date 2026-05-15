c4702ba1814532fa0097a33ceab5e72878784906
owo025893 <mkh08170601@gmail.com>
2026-05-15 17:35:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const mineflayer = require('mineflayer')
-const bot = mineflayer.createBot({
-    host: 'Youtube-ng80.aternos.me',
-    port: 19132,
-    username: 'bot',
-    version: false
-})
-bot.on('spawn', () => console.log('OK'))
-bot.on('end', () => process.exit())
-
+      - run: |
+          echo '{"dependencies":{"mineflayer":"latest","bedrock-protocol":"latest"},"scripts":{"start":"node index.js"}}' > package.json
+          cat <<EOF > index.js
+          const mineflayer = require('mineflayer');
+          const bot = mineflayer.createBot({
+              host: 'Youtube-ng80.aternos.me',
+              port: 26773,
+              username: 'bot',
+              version: '1.26.21.1'
+          });
+          bot.on('spawn', () => console.log('OK'));
+          bot.on('error', (err) => console.log('Error:', err.message));
+          EOF
